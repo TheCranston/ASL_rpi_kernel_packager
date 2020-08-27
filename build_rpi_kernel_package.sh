7992001ff210f1bc57371a8c67607b0d9c1d65cc
Adam Paul <adamjpaul@gmail.com>
2020-08-27 14:59:14 -0400

    initial commit

---
+++ build_rpi_kernel_package.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+TAG=$1
+if [ -z "$TAG" ] ; then
+  echo "please provide a tag"
+  exit 1
+fi
+
+#make note of the current directory so we can return back after the script
+ORIG_DIR="$(pwd)"
+
+#get the directory the script is in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+#get the cross compilation tools
+cd "$DIR"
+[ -d tools ] || git clone https://github.com/raspberrypi/tools
+export PATH="$PATH:$PWD/tools/arm-bcm2708/arm-linux-gnueabihf/bin"
+
+#get the required suplemental libraries for arm
+[ -d includes ] || mkdir includes
+cd includes
+if [ ! -d usr/include/arm-linux-gnueabihf ] ; then
+  wget http://raspbian.raspberrypi.org/raspbian/pool/main/g/glibc/libc6-dev_2.28-10+rpi1_armhf.deb
+  ar p libc6-dev_2.28-10+rpi1_armhf.deb data.tar.xz | tar xJf -
+  [ ! -d usr/include/arm-linux-gnueabihf ] && echo "unable to get libraries" && exit 1
+  rm libc6-dev_2.28-10+rpi1_armhf.deb
+fi
+if [ ! -d usr/include/openssl ] ; then
+  wget http://raspbian.raspberrypi.org/raspbian/pool/main/o/openssl/libssl-dev_1.1.1d-0+deb10u3_armhf.deb
+  ar p libssl-dev_1.1.1d-0+deb10u3_armhf.deb data.tar.xz | tar xJf -
+  [ ! -d usr/include/openssl ] && echo "unable to get libraries" && exit 1
+  rm libssl-dev_1.1.1d-0+deb10u3_armhf.deb
+fi
+exit
+
+#if the repository has already been cloned, update it.  otherwise, clone it
+if [ -d "$DIR/linux" ] ; then
+  cd "$DIR/linux"
+  git fetch
+else
+  cd "$DIR"
+  git clone https://github.com/raspberrypi/linux
+  cd linux
+fi
+
+#switch to the tag
+git checkout $TAG
+CUR_TAG=$(git describe --tags)  
+
+#if we're not on the desired tag, exit with error
+if [ "$CUR_TAG" != "$TAG" ] ; then
+  echo "Unable to find tag: $TAG"
+  exit 1
+fi
+
+KVERS="$(make kernelversion)"
+TAGVERS=$(echo $TAG | sed 's/raspberrypi-kernel_//g')
+
+#work around the deb-pkg bug when cross-compiling
+#and
+#change the kernel image filename to be rpi-compatible
+cd scripts/package
+patch < "$DIR/builddeb.patch"
+cd ../..
+
+
+make clean
+[ -d debian ] && rm -rf debian
+[ -f .version ] && rm .version
+KERNEL=kernel
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcmrpi_defconfig
+$DIR/customizations.sh
+#make -j4 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bindeb-pkg
+
+make clean
+[ -d debian ] && rm -rf debian
+[ -f .version ] && rm .version
+KERNEL=kernel7
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2709_defconfig
+$DIR/customizations.sh
+#make -j4 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bindeb-pkg
+
+make clean
+[ -d debian ] && rm -rf debian
+[ -f .version ] && rm .version
+KERNEL=kernel7l
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2711_defconfig
+$DIR/customizations.sh
+#make -j4 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bindeb-pkg
+
+#revert the Makefile back to the original
+git checkout Makefile
+
+#revert the builddeb file back to the original
+git checkout scripts/package/builddeb
+
+
+#set up metapackages
+cd "$DIR"
+sed -i "s/^Depends:.*/Depends: linux-image-$KVERS-asl+,linux-image-$KVERS-v7-asl+,linux-image-$KVERS-v7l-asl+/g" asl-raspberrypi-kernel-metapackage/asl-raspberrypi-kernel
+sed -i "s/^Depends:.*/Depends: linux-headers-$KVERS-asl+,linux-headers-$KVERS-v7-asl+,linux-headers-$KVERS-v7l-asl+/g" asl-raspberrypi-kernel-headers-metapackage/asl-raspberrypi-kernel-headers
+sed -i "s/^Version:.*/Version: $TAGVERS/g" asl-raspberrypi-kernel-metapackage/asl-raspberrypi-kernel
+sed -i "s/^Version:.*/Version: $TAGVERS/g" asl-raspberrypi-kernel-headers-metapackage/asl-raspberrypi-kernel-headers
+
+#build metapackages
+equivs-build asl-raspberrypi-kernel-metapackage/asl-raspberrypi-kernel
+equivs-build asl-raspberrypi-kernel-headers-metapackage/asl-raspberrypi-kernel-headers
+
+#reset metapackage control files
+sed -i "s/^Depends:.*/Depends: /g" asl-raspberrypi-kernel-metapackage/asl-raspberrypi-kernel
+sed -i "s/^Depends:.*/Depends: /g" asl-raspberrypi-kernel-headers-metapackage/asl-raspberrypi-kernel-headers
+sed -i "s/^Version:.*/Version: /g" asl-raspberrypi-kernel-metapackage/asl-raspberrypi-kernel
+sed -i "s/^Version:.*/Version: /g" asl-raspberrypi-kernel-headers-metapackage/asl-raspberrypi-kernel-headers
+
+#return back to the original directory
+cd $ORIG_DIR
---
+++ recompile_scripts.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -x
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/basic/.fixdep.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89         -o scripts/basic/fixdep scripts/basic/fixdep.c   
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/dtc/.dtc.o.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I ./scripts/dtc/libfdt -DNO_YAML  -c -o scripts/dtc/dtc.o scripts/dtc/dtc.c
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/dtc/.flattree.o.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I ./scripts/dtc/libfdt -DNO_YAML  -c -o scripts/dtc/flattree.o scripts/dtc/flattree.c
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/dtc/.fstree.o.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I ./scripts/dtc/libfdt -DNO_YAML  -c -o scripts/dtc/fstree.o scripts/dtc/fstree.c
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/dtc/.data.o.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I ./scripts/dtc/libfdt -DNO_YAML  -c -o scripts/dtc/data.o scripts/dtc/data.c
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/dtc/.livetree.o.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I ./scripts/dtc/libfdt -DNO_YAML  -c -o scripts/dtc/livetree.o scripts/dtc/livetree.c
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/dtc/.treesource.o.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I ./scripts/dtc/libfdt -DNO_YAML  -c -o scripts/dtc/treesource.o scripts/dtc/treesource.c
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/dtc/.srcpos.o.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I ./scripts/dtc/libfdt -DNO_YAML  -c -o scripts/dtc/srcpos.o scripts/dtc/srcpos.c
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/dtc/.checks.o.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I ./scripts/dtc/libfdt -DNO_YAML  -c -o scripts/dtc/checks.o scripts/dtc/checks.c
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/dtc/.util.o.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I ./scripts/dtc/libfdt -DNO_YAML  -c -o scripts/dtc/util.o scripts/dtc/util.c
+flex -oscripts/dtc/dtc-lexer.lex.c -L scripts/dtc/dtc-lexer.l
+bison -o scripts/dtc/dtc-parser.tab.c --defines=scripts/dtc/dtc-parser.tab.h -t -l scripts/dtc/dtc-parser.y
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/dtc/.dtc-lexer.lex.o.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I ./scripts/dtc/libfdt -DNO_YAML -I ./scripts/dtc -c -o scripts/dtc/dtc-lexer.lex.o scripts/dtc/dtc-lexer.lex.c
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/dtc/.dtc-parser.tab.o.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I ./scripts/dtc/libfdt -DNO_YAML -I ./scripts/dtc -c -o scripts/dtc/dtc-parser.tab.o scripts/dtc/dtc-parser.tab.c
+arm-linux-gnueabihf-gcc   -o scripts/dtc/dtc scripts/dtc/dtc.o scripts/dtc/flattree.o scripts/dtc/fstree.o scripts/dtc/data.o scripts/dtc/livetree.o scripts/dtc/treesource.o scripts/dtc/srcpos.o scripts/dtc/checks.o scripts/dtc/util.o scripts/dtc/dtc-lexer.lex.o scripts/dtc/dtc-parser.tab.o   
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/genksyms/.genksyms.o.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89       -c -o scripts/genksyms/genksyms.o scripts/genksyms/genksyms.c
+bison --version >/dev/null; bison -o scripts/genksyms/parse.tab.c --defines=scripts/genksyms/parse.tab.h -t -l scripts/genksyms/parse.y 2>/dev/null
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/genksyms/.parse.tab.o.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89      -I ./scripts/genksyms -c -o scripts/genksyms/parse.tab.o scripts/genksyms/parse.tab.c
+flex -oscripts/genksyms/lex.lex.c -L scripts/genksyms/lex.l
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/genksyms/.lex.lex.o.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89      -I ./scripts/genksyms -c -o scripts/genksyms/lex.lex.o scripts/genksyms/lex.lex.c
+arm-linux-gnueabihf-gcc   -o scripts/genksyms/genksyms scripts/genksyms/genksyms.o scripts/genksyms/parse.tab.o scripts/genksyms/lex.lex.o   
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/.kallsyms.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I./tools/include    -o scripts/kallsyms scripts/kallsyms.c   
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/.pnmtologo.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I./tools/include    -o scripts/pnmtologo scripts/pnmtologo.c   
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/.conmakehash.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I./tools/include    -o scripts/conmakehash scripts/conmakehash.c   
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/.recordmcount.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I./tools/include    -o scripts/recordmcount scripts/recordmcount.c   
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/.sortextable.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I./tools/include -I./tools/include   -o scripts/sortextable scripts/sortextable.c   
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/.asn1_compiler.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I./tools/include -I./include   -o scripts/asn1_compiler scripts/asn1_compiler.c   
+arm-linux-gnueabihf-gcc -Wp,-MD,scripts/.extract-cert.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89     -I./tools/include  -I../includes/usr/include -I../includes/usr/include/arm-linux-gnueabihf -I../includes/usr/lib/arm-linux-gnueabihf -o scripts/extract-cert scripts/extract-cert.c  -lcrypto -pthread -ldl
